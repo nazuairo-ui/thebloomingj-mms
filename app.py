e43c0d4b050d0a1e4b5a1dcd9d1be030a8bf9b9c
Naila <nazuairo@gmail.com>
2026-07-15 10:44:44 +0700

    ini pertama

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, render_template, request, url_for, jsonify, send_file, flash, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from flask import request, jsonify
+import uuid
+import os
+import io
+import csv
+import base64
+import re
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'tiket.db')
+app.config['SECRET_KEY'] = 'mms-kajian-secret-key-2026'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
