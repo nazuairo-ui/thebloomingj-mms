e57963c44fc10640da5132b2d84c3ecb7158c7c3
Naila <nazuairo@gmail.com>
2026-07-21 10:58:44 +0700

    sementara

--- app.py
+++ app.py
@@ -21,6 +21,17 @@ app.config['SECRET_KEY'] = 'mms-kajian-secret-key-2026'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Silakan login terlebih dahulu.'
+login_manager.login_message_category = 'warning'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/')
 def index():
@@ -30,6 +41,30 @@ def index():
 @app.route('/daftar')
 def pendaftaran():
     return render_template('daftar.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('admin'))
+        flash('Username atau password salah!', 'danger')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Berhasil logout.', 'info')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
